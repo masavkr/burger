a0022fd7b28d19f7f9b47de412a964666b90d634
Masa Venta <masaventa@3fs.si>
2019-06-26 09:37:42 +0200

    can add ingredients with the more button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import classes from './BuildControl.css'
+import classes from './BuildControl.module.css'
 
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
-    <d className={classes.Label}>{props.label}</d>
+    <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button className={classes.More} onClick={props.added}>More</button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Cheese', type: 'cheese'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Meat', type: 'meat'}
+];
+
 
 const buildControls = (props) => (
-  <div>
-
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        />
+    ))}
   </div>
 );
 
