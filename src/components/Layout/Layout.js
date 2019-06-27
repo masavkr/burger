8dd34293d25e522be03d1b58aa28da78198b7ece
Masa Venta <masaventa@3fs.si>
2019-06-27 10:07:03 +0200

    made it available for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import classes from './Layout.css'
+import classes from './Layout.module.css'
 import Aux from '../../hoc/Aux'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = ( props ) => (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
-      <main className={classes.content}>
+      <Toolbar />
+      <SideDrawer />
+      <main className={classes.Content}>
           {props.children}
       </main>
     </Aux>
